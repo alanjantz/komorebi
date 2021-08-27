27961d439ecb5fa30c4cfc0ec1aae43298a299d6
Alan Jantz <alan_f_jantz@hotmail.com>
2021-08-27 15:55:51 -0300

    Base estilo tema

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,10 @@
 import React from 'react';
 import { PageProps } from 'gatsby';
-
-import Title from '@/components/Title';
+import Layout from 'components/Layout';
 
 const Home: React.FC<PageProps> = () => (
   <main>
-    <Title>Hello TypeScript!</Title>
-    <p>A TypeScript starter for Gatsby. Great for advanced users.</p>
-    <p>
-      Follow me on Twitter (
-      <a href="https://twitter.com/jpedroschmitz">@jpedroschmitz</a>)
-    </p>
+    <Layout>Olá, tudo bem?</Layout>
   </main>
 );
 
